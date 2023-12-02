45613d21e39b8a6257370ed81ca179c7ef831c3a
kholoud001 <kholoud.sanak@gmail.com>
2023-12-02 22:24:50 +0100

    Create tables update

--- includes/create_tables.php
+++ includes/create_tables.php
@@ -17,11 +17,10 @@ CREATE TABLE IF NOT EXISTS User (
 
 // Execute the SQL query to create the table
 if (mysqli_query($conn, $sql_create_table_user)) {
-    echo "Table User created successfully";
+    echo "Table User created successfully" . " <br>";
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
+    echo "Error creating table: " . mysqli_error($conn) . " <br>";
 }
-
 
 // SQL to create table article
 $sql_create_table_article = "
@@ -33,14 +32,15 @@ CREATE TABLE IF NOT EXISTS Article (
     article_picture VARCHAR(50) NOT NULL,
     article_date VARCHAR(50) NOT NULL,
     creator_id INT(6) UNSIGNED,
-    FOREIGN KEY (creator_id) REFERENCES User(id_user)
+    FOREIGN KEY (creator_id) REFERENCES User(id_user) ON UPDATE CASCADE ON DELETE CASCADE
 )";
+
 
 // Execute the SQL query to create the table
 if (mysqli_query($conn, $sql_create_table_article)) {
-    echo "Table Article created successfully";
+    echo "Table Article created successfully" . " <br>";
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
+    echo "Error creating table: " . mysqli_error($conn) . " <br>";
 }
 
 // SQL to create table Comment
@@ -50,17 +50,16 @@ CREATE TABLE IF NOT EXISTS Comment (
     text_cmt VARCHAR(500) NOT NULL,
     date_cmt VARCHAR(50) NOT NULL,
     creator_id INT(6) UNSIGNED,
-    FOREIGN KEY (creator_id) REFERENCES User(id_user)
+    FOREIGN KEY (creator_id) REFERENCES User(id_user) ON UPDATE CASCADE ON DELETE CASCADE,
     article_id INT(6) UNSIGNED,
-    FOREIGN KEY (article_id) REFERENCES Article(id_article)
-    
+    FOREIGN KEY (article_id) REFERENCES Article(id_article) ON UPDATE CASCADE ON DELETE CASCADE
 )";
 
 // Execute the SQL query to create the table
 if (mysqli_query($conn, $sql_create_table_comment)) {
-    echo "Table Comment created successfully";
+    echo "Table Comment created successfully" . " <br>";
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
+    echo "Error creating table: " . mysqli_error($conn) . " <br>";
 }
 
 // SQL to create table Role
@@ -69,14 +68,15 @@ CREATE TABLE IF NOT EXISTS Role (
     id_role INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     role_title VARCHAR(50) NOT NULL,
     role_id INT(6) UNSIGNED,
-    FOREIGN KEY (role_id) REFERENCES User(id_user)
+    FOREIGN KEY (role_id) REFERENCES User(id_user) ON UPDATE CASCADE ON DELETE CASCADE
 )";
+
 
 // Execute the SQL query to create the table
 if (mysqli_query($conn, $sql_create_table_role)) {
-    echo "Table Role created successfully";
+    echo "Table Role created successfully" . " <br>";
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
+    echo "Error creating table: " . mysqli_error($conn) . " <br>";
 }
 
 // SQL to create table Permission
@@ -85,31 +85,35 @@ CREATE TABLE IF NOT EXISTS Permission (
     id_permission INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     permission_type VARCHAR(50) NOT NULL,
     user_id INT(6) UNSIGNED,
-    FOREIGN KEY (user_id) REFERENCES User(id_user)
+    FOREIGN KEY (user_id) REFERENCES User(id_user) ON UPDATE CASCADE ON DELETE CASCADE
 )";
+
 
 // Execute the SQL query to create the table
 if (mysqli_query($conn, $sql_create_table_permission)) {
-    echo "Table Permission created successfully";
+    echo "Table Permission created successfully" . " <br>";
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
+    echo "Error creating table: " . mysqli_error($conn) . " <br>";
 }
+
 
 // SQL to create table associative User Permission
 $sql_create_table_user_permission = "
 CREATE TABLE IF NOT EXISTS User_Permission (
-    user_id INT,
-    permission_id INT,
-    PRIMARY KEY (user_id, permission_id),
-    FOREIGN KEY (user_id) REFERENCES users(id_user) ON DELETE CASCADE,
-    FOREIGN KEY (permission_id) REFERENCES permissions(id_permission) ON DELETE CASCADE
+    id_user INT(6) UNSIGNED,
+    id_permission INT(6) UNSIGNED,
+    PRIMARY KEY (id_user, id_permission),
+    INDEX (id_user),
+    INDEX (id_permission),
+    FOREIGN KEY (id_user) REFERENCES User(id_user) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (id_permission) REFERENCES Permission(id_permission) ON UPDATE CASCADE ON DELETE CASCADE
 )";
 
 // Execute the SQL query to create the table
 if (mysqli_query($conn, $sql_create_table_user_permission)) {
-    echo "Table User_Permission created successfully";
+    echo "Table User_Permission created successfully" . " <br>";
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
+    echo "Error creating table: " . mysqli_error($conn) . " <br>";
 }
 
 // SQL to create table PasswordRecovery
@@ -121,14 +125,14 @@ CREATE TABLE IF NOT EXISTS PasswordRecovery (
     pwd_reset_token TEXT NOT NULL,
     pwd_reset_expires TEXT NOT NULL, 
     user_id INT(6) UNSIGNED,
-    FOREIGN KEY (user_id) REFERENCES User(id_user)
+    FOREIGN KEY (user_id) REFERENCES User(id_user)  ON UPDATE CASCADE ON DELETE CASCADE
 )";
 
 // Execute the SQL query to create the table
 if (mysqli_query($conn, $sql_create_table_password_recovery)) {
-    echo "Table PasswordRecovery created successfully";
+    echo "Table PasswordRecovery created successfully" . " <br>";
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
+    echo "Error creating table: " . mysqli_error($conn) . " <br>";
 }
 
 
