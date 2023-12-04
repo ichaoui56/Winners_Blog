602ea8f0d78d09ebae2a6d05b6bdb675748043d4
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-04 12:41:33 +0100

    testing the deployment

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                             src="https://dummyimage.com/722x402" alt="blog" style="filter: invert(0);">
                         <div class="p-6">
                             <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
+                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Live deploy</h1>
                             <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings
                                 jianbing microdosing tousled waistcoat.</p>
                             <div class="flex items-center flex-wrap ">
