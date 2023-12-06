57b2ddbb7a8687aabcfec0cf8637d2833cac51f8
oussamaef06 <oussamaelfarihi@gmail.com>
2023-12-06 16:24:05 +0100

    change categorie

--- pages/add_article.php
+++ pages/add_article.php
@@ -70,8 +70,8 @@
                             <div class="mt-2">
                                 <select id="" for="" name=""
                                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
+                                    <option value="1">phone</option>
+                                    <option value="2">laptop</option>
                                 </select>
                             </div>
                         </div>
