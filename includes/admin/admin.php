0ca9e2c35d395d847ad2f595582d69a541052dae
alifertah <alifertah42@gmail.com>
2023-12-06 10:56:01 +0100

    feat : data table styled in admin dashboard

--- includes/admin/admin.php
+++ includes/admin/admin.php
@@ -1,7 +1,6 @@
 <?php 
     include '../db.inc.php';
     $result = $conn->query("SELECT * FROM `user`;");
-   
 ?>
 
 <!DOCTYPE html>
@@ -10,38 +9,157 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
     <title>Document</title>
 </head>
-<body>
-<table id="myTable" class="display">
-    <thead>
-        <tr>
-            <th>username</th>
-            <th>Email</th>
-            <th>Phone</th>
-        </tr>
-    </thead>
+<body class="bg-gray-100 text-gray-900 tracking-wider leading-normal">
+<div class="container w-full md:w-4/5 xl:w-3/5  mx-auto px-2">
 
-    
-    <tbody>
-        <?php 
-            while( $col = $result->fetch_assoc()):?>
-           
+<!--Title-->
+<h1 class="flex items-center font-sans font-bold break-normal text-indigo-500 px-2 py-8 text-xl md:text-2xl">
+    ALL USERS
+</h1>
+
+
+<!--Card-->
+<div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
+
+
+    <table id="example" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
+        <thead>
             <tr>
-               <td> <?= $col["user_name"] ?></td>
-               <td> <?= $col["user_email"] ?></td>
-               <td> <?= $col["user_phone"] ?></td>
+                <th data-priority="1">ID</th>
+                <th data-priority="2">USERNAME</th>
+                <th data-priority="3">EMAIL</th>
+                <th data-priority="4">PHONE</th>
             </tr>
-            <?php endwhile;?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
 
+            <?php while($col = $result->fetch_assoc()):?>
+            <tr>
+                <td><?php echo $col["id_user"]; ?></td>
+                <td><?php echo $col["user_name"]; ?></td>
+                <td><?php echo $col["user_email"]; ?></td>
+                <td><?php echo $col["user_phone"]; ?></td>
+            </tr>
+            <?php endwhile; ?>
+
+        </tbody>
+
+    </table>
+
+
+</div>
+<!--/Card-->
+
+
+</div>
+<!--/container-->
 </body>
 <script>
+		$(document).ready(function() {
 
-   let table = new DataTable('#myTable', {
-   });
-</script>
+			var table = $('#example').DataTable({
+					responsive: true
+				})
+				.columns.adjust()
+				.responsive.recalc();
+		});
+	</script>
+
+
+<style>
+		/*Overrides for Tailwind CSS */
+
+		/*Form fields*/
+		.dataTables_wrapper select,
+		.dataTables_wrapper .dataTables_filter input {
+			color: #4a5568;
+			/*text-gray-700*/
+			padding-left: 1rem;
+			/*pl-4*/
+			padding-right: 1rem;
+			/*pl-4*/
+			padding-top: .5rem;
+			/*pl-2*/
+			padding-bottom: .5rem;
+			/*pl-2*/
+			line-height: 1.25;
+			/*leading-tight*/
+			border-width: 2px;
+			/*border-2*/
+			border-radius: .25rem;
+			border-color: #edf2f7;
+			/*border-gray-200*/
+			background-color: #edf2f7;
+			/*bg-gray-200*/
+		}
+
+		/*Row Hover*/
+		table.dataTable.hover tbody tr:hover,
+		table.dataTable.display tbody tr:hover {
+			background-color: #ebf4ff;
+			/*bg-indigo-100*/
+		}
+
+		/*Pagination Buttons*/
+		.dataTables_wrapper .dataTables_paginate .paginate_button {
+			font-weight: 700;
+			/*font-bold*/
+			border-radius: .25rem;
+			/*rounded*/
+			border: 1px solid transparent;
+			/*border border-transparent*/
+		}
+
+		/*Pagination Buttons - Current selected */
+		.dataTables_wrapper .dataTables_paginate .paginate_button.current {
+			color: #fff !important;
+			/*text-white*/
+			box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
+			/*shadow*/
+			font-weight: 700;
+			/*font-bold*/
+			border-radius: .25rem;
+			/*rounded*/
+			background: #667eea !important;
+			/*bg-indigo-500*/
+			border: 1px solid transparent;
+			/*border border-transparent*/
+		}
+
+		/*Pagination Buttons - Hover */
+		.dataTables_wrapper .dataTables_paginate .paginate_button:hover {
+			color: #fff !important;
+			/*text-white*/
+			box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
+			/*shadow*/
+			font-weight: 700;
+			/*font-bold*/
+			border-radius: .25rem;
+			/*rounded*/
+			background: #667eea !important;
+			/*bg-indigo-500*/
+			border: 1px solid transparent;
+			/*border border-transparent*/
+		}
+
+		/*Add padding to bottom border */
+		table.dataTable.no-footer {
+			border-bottom: 1px solid #e2e8f0;
+			/*border-b-1 border-gray-300*/
+			margin-top: 0.75em;
+			margin-bottom: 0.75em;
+		}
+
+		/*Change colour of responsive icon*/
+		table.dataTable.dtr-inline.collapsed>tbody>tr>td:first-child:before,
+		table.dataTable.dtr-inline.collapsed>tbody>tr>th:first-child:before {
+			background-color: #667eea !important;
+			/*bg-indigo-500*/
+		}
+	</style>
 </html>
