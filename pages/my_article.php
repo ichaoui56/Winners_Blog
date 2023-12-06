1c25e9d45b1973fc1e82b817aed0ea9ba7a56032
oussamaef06 <oussamaelfarihi@gmail.com>
2023-12-06 21:10:48 +0100

    creat my article page

--- pages/my_article.php
+++ pages/my_article.php
@@ -63,6 +63,13 @@
         <h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
       </a>
+      <a href="#" class="group">
+        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+          <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75">
+        </div>
+        <h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
+        <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
+      </a>
 
       <!-- More products... -->
     </div>
