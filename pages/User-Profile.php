b918818a357f7b0df0e3316efdfda32167ba5dae
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-08 12:16:21 +0100

    updating user profile

--- pages/User-Profile.php
+++ pages/User-Profile.php
@@ -1,4 +1,22 @@
-<?php include("../includes/db.inc.php") ?>
+<?php include("../includes/db.inc.php");
+
+require("../includes/utils/fetchData.php");
+
+session_start();
+if (isset($_SESSION["user_id"])) {
+    $userId = $_SESSION["user_id"];
+    $userInfo = getSpecificUser($userId, $conn);
+    $username = $userInfo["user_name"];
+    $userpic = $userInfo["user_picture"];
+    $userphone = $userInfo["user_phone"];
+    $useremail = $userInfo["user_email"];
+
+
+    // $userCity = $userInfo[""]
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -36,20 +54,17 @@
 
     <!-- component -->
     <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
-    <link rel="stylesheet"
-        href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
 
     <section class="pt- mx-10 mb-20 bg-transparent">
         <div class="w-full flex justify-center px-4 mx-auto">
-            <div class="lwa3er w-[1000px] z-10 flex flex-col break-words bg-white opacity-90  mb-6 shadow-xl rounded-lg mt-16" class="absolute flex flex-col min-w-0 break-words w-full bg-white opacity-90  mb-6 shadow-xl rounded-lg mt-16"
-                style="margin-top: 100px;">
+            <div class="lwa3er w-[1000px] z-10 flex flex-col break-words bg-white opacity-90  mb-6 shadow-xl rounded-lg mt-16" class="absolute flex flex-col min-w-0 break-words w-full bg-white opacity-90  mb-6 shadow-xl rounded-lg mt-16" style="margin-top: 100px;">
                 <div class="px-6">
                     <div class="flex flex-wrap justify-center">
                         <div class="w-full px-4 flex justify-center">
                             <div class="relative">
-                                <img alt="..."
-                                    src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg"
-                                    class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px">
+                                <?php echo '<img src="data:image/png;base64,' . base64_encode($userPic) . '" alt="" class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px">';
+                                ?>
                             </div>
                         </div>
                         <div class="w-full px-4 text-center ml-8 mt-20">
@@ -69,17 +84,27 @@
                             </div>
                         </div>
                     </div>
+
                     <div class="text-center mt-12">
+                        <a href="./EditUserProfile.php" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group">
+                            <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                                </svg>
+                            </span>
+                            <span class="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">Edit</span>
+                            <span class="relative invisible">Button</span>
+                        </a>
                         <h3 class="text-xl py-4 font-bold leading-normal mb-2 text-blueGray-700 mb-2">
-                            Jenna Stones
+                            <?php echo $userInfo["user_name"] ?>
                         </h3>
                         <div class="text-sm py-2 leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
                             <i class="fas fa-phone mr-2 text-lg text-blueGray-400"></i>
-                            Phone number
+                            <?php echo $userInfo["user_phone"] ?>
                         </div>
                         <div class="mb-2 text-blueGray-600 mt-10">
                             <i class="fas fa-mail-bulk mr-2 text-lg text-blueGray-400"></i>
-                            Email
+                            <?php echo $userInfo["user_email"] ?>
                         </div>
                         <div class="mb-2 py-10 text-blueGray-600">
                             <i class="fas fa-city mr-2 text-lg text-blueGray-400"></i>
@@ -96,8 +121,7 @@
 
     <div class="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
         <div>
-            <a title="Buy me a beer" href="https://www.avito.ma/" target="_blank"
-                class="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
+            <a title="Buy me a beer" href="https://www.avito.ma/" target="_blank" class="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
                 <img class="object-cover object-center w-full h-full rounded-full" src="../pictures/login_pic.png" />
             </a>
         </div>
