561dbbbfbfca7a43effa77248570d5c21da9d48a
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-08 16:10:04 +0100

    update soft delete second time

--- includes/profile-crud/delete_profile.php
+++ includes/profile-crud/delete_profile.php
@@ -4,21 +4,21 @@ include("../db.inc.php");
 
 session_start();
 
-
-
 if (isset($_SESSION["user_id"])) {
     $userId = $_SESSION["user_id"];
     $date = date("Y-m-d H:i:s");
-    $Delete_sql = "INSERT INTO User (soft_delete) VALUE '$date' WHERE id_user = ?";
+    $Update_sql = "UPDATE User SET soft_delete = ? WHERE id_user = ?";
 
-    $stmt = mysqli_prepare($conn, $Delete_sql);
-    mysqli_stmt_bind_param($stmt, "i",$userId);
+    $stmt = mysqli_prepare($conn, $Update_sql);
+    mysqli_stmt_bind_param($stmt, "si", $date, $userId);
     $result = mysqli_stmt_execute($stmt);
 
     if ($result) {
         session_destroy();
         header('location: ../../pages/login.php');
         exit();
+    } else {
+        echo "Error in updating account.";
     }
 } else {
     echo "Error in deleting account.";
