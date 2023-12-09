6063a388d9b12f59b1577d53f430a717dad3a9e9
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-09 15:19:09 +0100

    add two button foredit and delete in my article page

--- pages/my_article.php
+++ pages/my_article.php
@@ -9,13 +9,15 @@
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="../css/style.css">
   <link rel="icon" type="image/png" href="../pictures/avito.png" />
-  <title>Add Article</title>
+  <title>Avito</title>
 </head>
 
 <body class="bg-gray-300" style="background-color: #d5deef;">
 
   <!-- Navbar -->
-  <div id="navbar-container"><?php include("../js/navbar.php"); ?></div>
+  <div id="navbar-container">
+    <?php include("../js/navbar.php"); ?>
+  </div>
   <?php
   if (isset($_SESSION["user_id"])) {
     $userId = $_SESSION["user_id"];
@@ -50,38 +52,66 @@
         <?php
         foreach ($articles as $key => $value) {
         ?>
-          <div class="p-4 md:w-1/3">
+        <div class="p-4 md:w-1/3">
 
-            <div class="draggable h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden" draggable="true">
-              <?php echo '<img src="data:image/png;base64,' . base64_encode($value["article_picture"]) . '" alt="blog" style="filter: invert(0);" class="lg:h-48 md:h-36 w-full object-cover object-center"/>';
+          <div class="draggable h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden"
+            draggable="true">
+            <?php echo '<img src="data:image/png;base64,' . base64_encode($value["article_picture"]) . '" alt="blog" style="filter: invert(0);" class="sba3 lg:h-48 md:h-36 w-full object-contain object-center"/>';
               ?>
-              <div class="p-6">
-                <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"><?php echo $value["category"]; ?></h2>
-                <h1 class="title-font text-lg font-medium text-gray-900 mb-3"><?php echo $value["title"]; ?></h1>
-                <p class="leading-relaxed mb-3"><?php echo $value["description"]; ?></p>
-                <div class="flex items-center flex-wrap ">
-                  <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                      <path d="M5 12h14"></path>
-                      <path d="M12 5l7 7-7 7"></path>
-                    </svg>
+            <div class="lg:h-20 md:h-14 flex justify-center " style="backdrop-filter: blur(10px);">
+              <button>
+                  <a href="#_"
+                    class="relative rounded  px-5 py-2.5 overflow-hidden group bg-green-500 hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300">
+                    <span
+                      class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+                    <span class="relative">Edit</span>
                   </a>
-                  <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                    <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                      <circle cx="12" cy="12" r="3"></circle>
-                    </svg>1.2K
-                  </span>
-                  <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                    <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
-                      </path>
-                    </svg>6
-                  </span>
-                </div>
+              </button>
+              <button>
+                <a href="#_"
+                  class="relative rounded px-5 ml-5 py-2.5 overflow-hidden group bg-red-500 hover:bg-gradient-to-r hover:from-red-500 hover:to-red-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-red-400 transition-all ease-out duration-300">
+                  <span
+                    class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+                  <span class="relative">Delete</span>
+                </a>
+              </button>
+            </div>
+
+            <div class="p-6">
+              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                <?php echo $value["category"]; ?>
+              </h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                <?php echo $value["title"]; ?>
+              </h1>
+              <p class="leading-relaxed mb-3">
+                <?php echo $value["description"]; ?>
+              </p>
+              <div class="flex items-center flex-wrap ">
+                <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                  <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+                <span
+                  class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                  <?php echo $value["article_date"]
+                    ?>
+                </span>
+                <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                  <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round"
+                    stroke-linejoin="round" viewBox="0 0 24 24">
+                    <path
+                      d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
+                    </path>
+                  </svg>6
+                </span>
               </div>
             </div>
           </div>
+        </div>
         <?php } ?>
       </div>
     </div>
