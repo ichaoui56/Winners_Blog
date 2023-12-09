d7c0f7732b5b6f01a3c4e25a37b9adcff7f23ecb
alifertah <alifertah42@gmail.com>
2023-12-09 16:32:25 +0100

    feat : adding comment creator id

--- pages/articleInsert.php
+++ pages/articleInsert.php
@@ -1,8 +1,13 @@
 <?php 
+    session_start();
     include "../includes/db.inc.php";
     $comment = $_POST["text_cmt"];
     $date_cmt = date('Y-m-d H:i:s');
-    $sql = "INSERT INTO comment (text_cmt, date_cmt) VALUES ('$comment', '$date_cmt')";
+    $userId = $_SESSION["user_id"];
+    echo $_SESSION["user_id"] . "jelllllll";
+    $sql = "INSERT INTO comment (text_cmt, date_cmt, creator_id) 
+    VALUES 
+     ('$comment', '$date_cmt', '$userId')";
     if ($conn->query($sql) === TRUE) {
         echo "Record inserted successfully";
     } else {
