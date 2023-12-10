15ebb3dd242e63cedebfeb7ca34299863365a9b0
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-10 22:25:19 +0100

    login admin page

---
+++ includes/admin/Login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <title>Admin</title>
+    <link rel="icon" type="image/png" href="../pictures/2.png" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="./../../css/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <div class="loginBox"> <img class="user" src="https://i.ibb.co/yVGxFPR/2.png" height="100px" width="100px">
+        <h3>HELLO ADMIN </h3>
+        <form action="" method="">
+            <div class="inputBox">
+                <input id="uname" type="text" name="Username" autocomplete="off" placeholder="Username">
+                <input id="pass" type="password" name="Password" autocomplete="off" placeholder="Password">
+                <button class="button"> Login
+                </button>
+        </form>
+    </div>
+</body>
+
+
+
+</html>
