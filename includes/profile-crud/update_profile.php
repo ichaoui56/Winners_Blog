c02ab8072e80c6fb590c0fe89d3a6199ffad9ded
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-10 13:26:02 +0100

    add city column to the update profile traitement

--- includes/profile-crud/update_profile.php
+++ includes/profile-crud/update_profile.php
@@ -11,6 +11,7 @@ if (isset($_SESSION["user_id"])) {
     $new_picture = $_FILES["newimage"]["tmp_name"];
     $username = $_POST['user_name'];
     $userphone = $_POST['user_phone'];
+    $usercity = $_POST['user_city'];
     $useremail = $_POST['user_email'];
 
     if (empty($new_picture)) {
@@ -20,9 +21,9 @@ if (isset($_SESSION["user_id"])) {
         $new_picture = file_get_contents($new_picture);
     }
 
-    $Update_sql = "UPDATE User SET user_name =?, user_phone=? ,user_email=? ,user_picture=? WHERE id_user=?";
+    $Update_sql = "UPDATE User SET user_name =?, user_phone=?, city=? ,user_email=? ,user_picture=? WHERE id_user=?";
     $stmt = mysqli_prepare($conn, $Update_sql);
-    mysqli_stmt_bind_param($stmt, "sissi", $username, $userphone, $useremail, $new_picture, $userId);
+    mysqli_stmt_bind_param($stmt, "sisssi", $username, $userphone, $usercity , $useremail, $new_picture, $userId);
     $result = mysqli_stmt_execute($stmt);
 
 
