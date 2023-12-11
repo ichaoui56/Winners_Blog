c88517dbab776be174107da678c76edd7ad24b16
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-11 20:32:52 +0100

    update db config

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,9 +1,10 @@
 <?php
+ini_set('display_errors', 1); ini_set('log_errors',1); error_reporting(E_ALL); mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 session_start();
-$db_server = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "blog";
+$db_server = "sql206.infinityfree.com";
+$db_user = "if0_35553201";
+$db_pass = "jS4FFzcVvx";
+$db_name = "if0_35553201_BlogDB";
 $conn = "";
 
 // Create connection & check connection
