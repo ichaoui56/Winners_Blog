4300caef25ca6f96463fabb14a980ccaa431d4f6
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-11 21:42:57 +0100

    change path

--- includes/profile-crud/delete_profile.php
+++ includes/profile-crud/delete_profile.php
@@ -25,7 +25,7 @@ if (isset($_SESSION["user_id"])) {
 
     if ($result) {
         session_destroy();
-        header('location: ../../pages/login.php');
+        header('location: ../../pages/login.php?deleted=true');
         exit();
     } else {
         echo "Error in updating account.";
