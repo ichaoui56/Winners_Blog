1be972f3d0f0cabcf3a53de75c906d4785021ad8
Anass Benfillous <anasse.benfillous@gmail.com>
2023-12-11 20:36:48 +0100

    update db config

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -13,8 +13,8 @@ try {
 } catch (mysqli_sql_exception) {
     echo "Not connected: ";
 }
-// Create Database
-$sql_create_db = "CREATE DATABASE IF NOT EXISTS blog";
+Create Database
+$sql_create_db = "CREATE DATABASE IF NOT EXISTS if0_35553201_blog";
 $req=mysqli_query($conn, $sql_create_db);
 
 // Check if the database is created
