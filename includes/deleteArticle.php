1469a12c26102db6b01178d331c376cd82f72b18
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-12-11 21:42:19 +0100

    correct delete traitement

--- includes/deleteArticle.php
+++ includes/deleteArticle.php
@@ -1,13 +1,23 @@
 <?php
 include("db.inc.php");
 if (isset($_GET["articleId"])) {
-    $articleId = filter_input(INPUT_GET, "articleId", FILTER_DEFAULT);
+   
+    $articleId = intval($_GET["articleId"]);
     $date = date("Y-m-d H:i:s");
-    $sql = "UPDATE article SET soft_delete='$date' WHERE id_article= '$articleId'";
+    $sql = "UPDATE article SET soft_delete='$date' WHERE id_article='$articleId'";
     $stmt = mysqli_stmt_init($conn);
+   
     mysqli_stmt_prepare($stmt, $sql);
-    mysqli_stmt_execute($stmt);
-    header("Location: ../pages/my_article.php?article=deleted");
+   
+    if(mysqli_stmt_execute($stmt)){
+        
+         header("Location: ../pages/my_article.php?artile=deleted");
+    }else{
+        echo "<script>alert('matamsa7ch')</script>";
+    }
+   
+
+   
 } else {
     header("Location: ../index.php");
 }
