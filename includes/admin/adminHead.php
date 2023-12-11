85b0a7644ce98a4f19770509fba59e75b226dce1
oussamaef06 <oussamaelfarihi@gmail.com>
2023-12-11 12:01:07 +0100

    creat files and pages needed

--- includes/admin/adminHead.php
+++ includes/admin/adminHead.php
@@ -6,7 +6,10 @@ echo(
             <a href='addUser.php'>new user</a>
             <a class='px-8' href='editUser.php'>edit user</a>
             <a href='delUser.php'>remove user</a>
-            <a class='pl-2' href='admin.php'>dashboard</a>
+            <a class='pl-2 px-8' href='admin.php'>dashboard</a>
+            <a href='addArticle.php'>add article</a>
+            <a class='pl-2 px-8' href='editArticle.php'>edit article</a>
+            <a href='delArticle.php'>remove article</a>
         </div>
         <form method='post'>
             <button type='submit' name='logout' >logout</button>
--- js/footer.js
+++ js/footer.js
@@ -260,7 +260,7 @@ function AddFooter() {
                 <div class="mt-8 border-t border-gray-100 pt-8">
                     <div class="sm:flex sm:justify-between">
                         <p class="text-xs text-gray-500">
-                            &copy; 2022. Company Name. All rights reserved.
+                            &copy; <2022>. Company Name. All rights reserved.
                         </p>
 
                         <ul class="mt-8 flex flex-wrap justify-start gap-4 text-xs sm:mt-0 lg:justify-end">
